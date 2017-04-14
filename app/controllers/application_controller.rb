5bf624b3e63a6f8b29e936eab8b7bf7ed6539160
Josip Škarpa <josip.skarpa@hotmail.com>
2017-04-14 08:54:16 +0000

    Fixed Stripe integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   # Whitelist the following form fields so that we can process them, if coming form
   # a Devise sign up form
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  protected
-    def configure_permitted_parameters
+protected
+  def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
-    end
+  end
 end
